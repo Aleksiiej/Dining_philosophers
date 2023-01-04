8ba7dbd19ac70d25aa0ad7c0915c9a14c636512e
Aleksiiej <aleksander.ramut@wp.pl>
2023-01-04 23:43:33 +0100

    Work over implementation completed

--- src/Philosopher.cpp
+++ src/Philosopher.cpp
@@ -7,16 +7,9 @@
 
 using namespace std::chrono_literals;
 
-
-enum class State
-{
-    THINKING,
-    EATING
-};
-
-constexpr uint8_t N = 5;
-std::vector<State> states{N, State::THINKING};
-std::mutex mt;
+constexpr uint8_t NUM_OF_PHILOSOPHERS = 5;
+std::array<std::mutex, NUM_OF_PHILOSOPHERS> mutexes;
+std::mutex iostream_mt;
 std::random_device rd;
 
 int getRandomTime()
@@ -26,51 +19,25 @@ int getRandomTime()
     return distrib(rng);
 }
 
-bool checkIfForksAreFree(const int i)
-{
-    std::lock_guard lg{mt};
-    if (states.size() == 2 and i == 0)
-    {
-        return (states.at(1) == State::THINKING);
-    }
-    else if (states.size() == 2 and i == 1)
-    {
-        return (states.at(0) == State::THINKING);
-    }
-    else if (i == 0)
-    {
-        return (states.at(1) == State::THINKING and states.at(states.size() - 1) == State::THINKING) ? true : false;
-    }
-    else if (static_cast<unsigned int>(i) == states.size() - 1)
-    {
-        return (states.at(i - 1) == State::THINKING and states.at(0) == State::THINKING) ? true : false;
-    }
-    else
-        return (states.at(i - 1) == State::THINKING and states.at(i + 1) == State::THINKING) ? true : false;
-}
-
 void think(const int i)
 {
     {
-        std::lock_guard lg{mt};
+        std::lock_guard lg{iostream_mt};
         std::cout << "Philosopher nr: " << i << " thinking..." << std::endl;
     }
-    while (!checkIfForksAreFree(i))
-    {
-    }
+    std::this_thread::sleep_for(static_cast<std::chrono::milliseconds>(getRandomTime()));
 }
 
 void takeFork(const int i)
 {
-    std::lock_guard<std::mutex> lg{mt};
+    std::lock_guard lg{iostream_mt};
     std::cout << "Philosopher nr: " << i << " taking fork..." << std::endl;
-    states.at(i) = State::EATING;
 }
 
 void eat(const int i)
 {
     {
-        std::lock_guard lg{mt};
+        std::lock_guard lg{iostream_mt};
         std::cout << "Philosopher nr: " << i << " eating..." << std::endl;
     }
     std::this_thread::sleep_for(static_cast<std::chrono::milliseconds>(getRandomTime()));
@@ -78,23 +45,26 @@ void eat(const int i)
 
 void putFork(const int i)
 {
-    std::lock_guard<std::mutex> lg{mt};
+    std::lock_guard lg{iostream_mt};
     std::cout << "Philosopher nr: " << i << " putting fork down..." << std::endl;
-    states.at(i) = State::THINKING;
 }
 
-auto philosopher = [portions = 5](const auto i) mutable
+auto philosopher = [totalPortions = 5, eatenPortions = 0](const auto i) mutable
 {
-    while (portions != 0)
+    while (totalPortions != 0)
     {
         think(i);
-        takeFork(i);
-        eat(i);
-        portions--;
-        putFork(i);
+        {
+            std::scoped_lock sl(mutexes.at(i), mutexes.at((i + 1) % 5));
+            takeFork(i);
+            eat(i);
+            totalPortions--;
+            eatenPortions++;
+            putFork(i);
+        }
     }
     {
-        std::lock_guard lg{mt};
-        std::cout << "Philosopher nr: " << i << " finished dinner" << std::endl;
+        std::lock_guard lg{iostream_mt};
+        std::cout << "Philosopher nr: " << i << " finished dinner, ate " << eatenPortions << " portions" << std::endl;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@ int main()
 {
     std::vector<std::thread> threads;
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < NUM_OF_PHILOSOPHERS; i++)
     {
         threads.push_back(std::thread(philosopher, i));
     }
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < NUM_OF_PHILOSOPHERS; i++)
     {
         threads.at(i).join();
     }
